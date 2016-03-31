865865fee4992f1ed3cc972623005cc51a9f60c5
Josephine Lee <leejosephines@wustl.edu>
2016-03-31 08:10:36 -0500

    fixed the content noshow problem. Go for it! :D

--- jQuery/display.js
+++ jQuery/display.js
@@ -1,4 +1,4 @@
-rooms = [];
+rooms = []; // make sure to push to this array whenever a room is made
 var max_room_id = 0; // increments every time a room is created
 
 // Hi Dylan!
@@ -8,12 +8,11 @@ var max_room_id = 0; // increments every time a room is created
 // Created the prototypes (classes) for Room and PrivateRoom, and added some methods for them.
 // To create them refer to line 98
 // Made buttons for rooms
-// Made a tab appear/be selected if the button is clicked <--- trying to figure out bug that content won't show
-// Made a tab be selected and its content (hardcoded in client.html) appear if it is clicked
+// Made a tab appear/be selected + show content (hardcoded in client.html) if the button is clicked
+// Made a tab be selected and its content appear if it is clicked
 // Also added some ideas for the creative in the readme
 //
-// Tomorrow morning when I ask Adam stuff I'll also see if he can help fix the bug about content not showing when a new tab is added
-// Otherwise, I won't touch it after 11.30am. Take it away!
+// I ask Adam about the creative later. Take it away!
 
 
 function Room(name, creator) { // base room constructor
@@ -21,17 +20,25 @@ function Room(name, creator) { // base room constructor
 	this.creator = creator;
 	this.users = [creator];
 	this.id = max_room_id;
-	max_room_id++;
+	max_room_id++; // auto increment the id
+	var roomButton = document.createElement("button"); // whenever a room is created, add a button for it
+			$('.chatrooms').append($(roomButton).attr({
+			class : "list-group-item",
+			value : this.name,
+			id : this.id,  // id of the room, used to identify tab and tab content
+			href :"#room_"+this.id,
+			'data-toggle' : "tab"
+		}).text(this.name)); 
 }
 
 Room.prototype = {
 	constructor: Room, // assign/(override?) constructor
-	addUser:function (user_to_add)  { // methods.
+	addUser:function (user_to_add)  { // methods. Add user to the users array in a room
 		this.users.push(user_to_add);
 		console.log(user_to_add+" added");
 	},
-	removeUser:function (user_to_remove)  {
-		var index = users.indexOf(user_to_remove);
+	removeUser:function (user_to_remove)  { // Remove user from the users array in a room
+		var index = users.indexOf(user_to_remove); 
 		if (index > -1) {
 			users.splice(index, 1);
 			console.log(user_to_remove+" removed");
@@ -43,74 +50,53 @@ Room.prototype = {
 }
 
 function PrivateRoom (name, creator, password, blacklist) {
-	Room.call(this, name, creator);
-	this.password = password;
-	if (typeof blacklist === 'undefined'){
+	Room.call(this, name, creator); // use the code from function Room
+	this.password = password; // also set a password
+	if (typeof blacklist === 'undefined'){ // initialize empty blacklist if not specified
 		this.blacklist = [];
 	}else{
-		this.blacklist = blacklist;
+		this.blacklist = blacklist; // else initialize blacklist
 	}	
 }
-var tmp = function(){}; // so that it doesn't copy the Room constructor. Refer to the second-best answer here:http://stackoverflow.com/questions/4152931/javascript-inheritance-call-super-constructor-or-use-prototype-chain
+var tmp = function(){}; // so that it doesn't copy the Room constructor. Refer to the second-best answer here: http://stackoverflow.com/questions/4152931/javascript-inheritance-call-super-constructor-or-use-prototype-chain
 tmp.prototype = Room.prototype;
-PrivateRoom.prototype = new tmp(); // inherit
+PrivateRoom.prototype = new tmp(); // inherit the prototypes and a blank constructor.
 PrivateRoom.prototype.constructor = PrivateRoom; //override constructor
-PrivateRoom.prototype.banUser = function (user_to_ban)  { // add banUser method
+PrivateRoom.prototype.banUser = function (user_to_ban)  { // add user to blacklist of a room
 	this.blacklist.push(user_to_ban);
 	console.log(user_to_ban+" banned");
 }	
 
-
-function showRoomButtons(){
-	for (var i = 0; i < rooms.length; i++){
-		var roomButton = document.createElement("button");
-		$(roomButton).attr({
-			class : "list-group-item",
-			value : rooms[i].name,
-			id : rooms[i].id // id is auto incremented whenever a new room is constructed
-		});
-		// how to add nested DOM with jquery: http://stackoverflow.com/questions/5317890/how-do-i-create-these-nested-dom-elements-with-jquery
-		// make sure to append any variable as text! same reason not to use innerHTML.
-		// inspect element to see what the HTML structure ultimately looks like
-		 $('.chatrooms').append($(roomButton).text(rooms[i].name).click(function(event){ // if click on the button
-		 	event.preventDefault();
-		 	var id = $(this).attr("id"); // get the id
-		 	if ($('#tab_'+id).length == 0){ // if tab doesn't already exist
-				var roomTab = $("<li>").attr('id','tab_'+id).append(// make and display new tab
-					$("<a>").attr({
-						href :"#room_"+id,
-						'data-toggle' : "tab"
-					}).text($(this).attr("value"))
-					);
+// Detect if tabs or buttons are clicked, if so show their content
+function addButtonTabListeners(){
+	$(".chatrooms").on("click", "button", function(event){
+		event.preventDefault();
+		var id = $(this).attr("id");
+		if ($('#tab_'+id).length == 0){ // if tab doesn't already exist
+			var roomTab = $("<li>").attr('id','tab_'+id).append(// make and display new tab
+				$("<a>").attr({
+					href :"#room_"+id,
+					'data-toggle' : "tab"
+				}).text($(this).attr("value"))
+			);
 			$('.nav-tabs').append(roomTab);
-			}
-			changeToTab(id); // switch to that tab
-		}));
-       	}
-       }
-
-function changeToTab(id){
-	$('ul.nav-tabs li.active').removeClass('active');
-	$('#tab_'+id).addClass('active');
-	$('#tab_'+id+' a').tab('show'); // Don't know why this isn't working. When a tab is first created and appears, the content does not show
-}
-
-// Referring to http://jsfiddle.net/dogoku/KdPdZ/2/
-// Detect if tabs are clicked, if so show their content
-function addjQueryListeners(){
-	$(".nav-tabs").on("click", "a", function(e){
-		e.preventDefault();
-		console.log("clicked");
+		}
+		$('ul.nav-tabs li.active').removeClass('active');
+		$('#tab_'+id).addClass('active');
+		$(this).tab('show');
+	});
+	$(".nav-tabs").on("click", "a", function(event){
+		event.preventDefault();
 		$(this).tab('show');
 	})
 }
 
 function doUponLoading(event){
+	addButtonTabListeners();
+	
+	// Where I test things out
 	rooms.push(new PrivateRoom("roomName", "theCreator", "abcd"));
 	rooms.push(new PrivateRoom("Sun", "Moon", "abcd"));
-	console.log("max num is"+max_room_id);
 	rooms.push(new PrivateRoom("hey", "hi", "abcd"));
 	console.log(rooms);
-	showRoomButtons();
-	addjQueryListeners();
 }
