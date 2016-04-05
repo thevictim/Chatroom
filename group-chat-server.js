57e1f46f1b0b6bf49ee65105f549e51af0d54ec0
Josephine Lee <leejosephines@wustl.edu>
2016-04-05 18:49:19 -0500

    param id stuff, gonna remove

--- group-chat-server.js
+++ group-chat-server.js
@@ -1,3 +1,91 @@
+// ******************
+// Stuff for rooms
+// ******************
+
+		rooms = []; // make sure to push to this array whenever a room is made
+		var max_room_id = 0; // increments every time a room is created
+		var nickname;
+
+		function Room(name, creator) { // base room constructor
+			this.name = name
+			this.creator = creator;
+			this.users = [creator];
+			this.id = max_room_id;
+		 	max_room_id++; // auto increment the id
+		// 	var roomButton = document.createElement("button"); // whenever a room is created, add a button for it
+ 
+		// 	$('.chatrooms').append($(roomButton).attr({
+		// 		class : "list-group-item",
+		// 		value : this.name,
+		// 	id : this.id,  // id of the room, used to identify tab and tab content
+		// 	href :"#room_"+this.id,
+		// 	'data-toggle' : "tab"
+		// }).text(this.name)); 
+		}
+
+		Room.prototype = {
+			constructor: Room, // assign/(override?) constructor
+			addUser:function (user_to_add)  { // methods. Add user to the users array in a room
+				this.users.push(user_to_add);
+				console.log(user_to_add+" added");
+				// FIXME refresh users in room 
+				// var userButton = document.createElement("button");
+				// $('.current_users').append($(userButton).attr({
+				// 	class : "list-group-item",
+				// 	value : user_to_add,
+				// 	// id : users[i],  // id of the room, used to identify tab and tab content
+				// 	// href :"#room_"+this.id,
+				// 	'data-toggle' : "tab"
+				// }).text(user_to_add));
+			},
+			removeUser:function (user_to_remove)  { // Remove user from the users array in a room
+				var index = users.indexOf(user_to_remove); 
+				if (index > -1) {
+					users.splice(index, 1);
+					console.log(user_to_remove+" removed");
+					if (users.length == 0){
+						console.log("no more users in room");
+					}
+				}
+			}
+		}
+
+		function PrivateRoom (name, creator, password, blacklist) {
+			Room.call(this, name, creator); // use the code from function Room
+			this.password = password; // also set a password
+			if (typeof blacklist === 'undefined'){ // initialize empty blacklist if not specified
+				this.blacklist = [];
+			}else{
+				this.blacklist = blacklist; // else initialize blacklist
+			}	
+		}
+		var tmp = function(){}; // so that it doesn't copy the Room constructor. Refer to the second-best answer here: http://stackoverflow.com/questions/4152931/javascript-inheritance-call-super-constructor-or-use-prototype-chain
+		tmp.prototype = Room.prototype;
+		PrivateRoom.prototype = new tmp(); // inherit the prototypes and a blank constructor.
+		PrivateRoom.prototype.constructor = PrivateRoom; //override constructor
+		PrivateRoom.prototype.banUser = function (user_to_ban){ // add user to blacklist of a room
+			this.blacklist.push(user_to_ban);
+			console.log(user_to_ban+" banned");
+		}	
+
+		function twoPersonRoom(creator, friend) { // base room constructor
+			for (var i = 0; i < rooms.length; i++){
+				if ((rooms[i].name == creator && rooms[i].creator == friend) || (rooms[i].name == friend && rooms[i].creator == creator)){
+					return rooms[i];
+				}
+			}
+			this.name = friend;
+			this.creator = creator;
+			this.users = [];
+			this.id = max_room_id;
+			max_room_id++; // auto increment the id
+			// var roomButton = document.createElement("button"); // whenever a room is created, add a button for it
+		}
+
+// ********************
+// Stuff for socket.io
+// ********************
+
 // Require the packages we will use:
 var http = require("http"),
 	socketio = require("socket.io"),
@@ -38,3 +126,7 @@ io.sockets.on("connection", function(socket){
 		 }) // broadcast the message to other users
 	});
 });
+
+
+
+
